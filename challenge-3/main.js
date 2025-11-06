b5b59e811e172f1c50584dc89d47224713a71ed0
Daniel Moreira <danniel.moreiira04@hotmail.com>
2025-11-06 01:03:49 -0300

    Finalizado
    Finalizei a validação de email e senha

--- challenge-3/main.js
+++ challenge-3/main.js
@@ -1,5 +1,7 @@
 const emailValidacao = document.getElementById("email");
 const senhaValidacao = document.getElementById("senha")
+const validacaoEmail = document.getElementById('emailValidacao');
+const validacaoSenha = document.getElementById('senhaValidacao');
 
 function isValidEmail (email) {
     const regex = /^[\s]+[\w.-]+@[\w.-]+\.\w+$/;
@@ -12,3 +14,32 @@ function isValidSenha (senha) {
     const numeros = /\d/;
     return tamanhoString && letras && numeros
 };
+
+validacaoEmail.addEventListener("input", () => {
+    if (isValidEmail(emailValidacao.value)) {
+        emailValidacao.classList.add('valid');
+        emailValidacao.classList.remove('invalid');
+        msg.textContent = "Email valido";
+        msg.style.color = "green";
+    } else {
+        emailValidacao.classList.add('invalid');
+        emailValidacao.classList.remove('valid');
+        msg.textContent = "Email invalido";
+        msg.style.color = "red";
+    }
+
+});
+
+validacaoSenha.addEventListener("input", () => {
+    if (isValidEmail(emailValidacao.value)) {
+        emailValidacao.classList.add('valid');
+        emailValidacao.classList.remove('invalid');
+        senhaMsg.textContent = "Senha forte";
+        senhaMsg.style.color = "green";
+    } else {
+        emailValidacao.classList.add('invalid');
+        emailValidacao.classList.remove('valid');
+        senhaMsg.textContent = "Senha fraca";
+        senhaMsg.style.color = "red";
+    }
+});
